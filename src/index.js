65a5c20166e0d2a0aa4713e7ed9a0a82e9c06227
Nikadzim Havarushkin <55991472+onjjje@users.noreply.github.com>
2020-03-22 18:17:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let min = array[0];
+    for (let i = 0; i < array.length; i += 1){
+        array[i] < min ? min = array[i] : min = min;
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let max = array[0];
+    for (let i = 0; i < array.length; i += 1){
+        array[i] > max ? max = array[i] : max = max;
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) return 0;
+    let avg = 0;
+    for (let i = 0; i < array.length; i += 1){
+        avg += array[i];
+    }
+    return avg / array.length;
 }
